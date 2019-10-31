be439767db0dc6bb362a52c1318fe55f55902274
Sodiq <sodiqlawal95@gmail.com>
2019-10-31 16:36:08 +0100

    created the modals

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import axios from 'axios';
 
 
@@ -6,28 +7,89 @@ export default class Display extends React.Component {
     constructor(props){
         super(props)
         this.state={
-            data:[]
+            data:[],
+            newUserCreateDataModal: false,
+            newUserDisplayDataModal: false
         }
     }
 
 
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => this.setState({data:response})) 
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => this.setState({data:response})) 
     }
+
+    toggleNewUserCreateDataModal = () => {
+        this.setState({
+          newUserCreateDataModal:true
+        });
+      }
+
+      toggleNewUserDisplayDataModal = () => {
+        this.setState({
+          newUserDisplayDataModal:true
+        });
+      }
 
     render(){
         console.log(this.state.data)
         return(
             <React.Fragment>
                 <div  className='container-fluid pt4'>
+                {/* start of create modal */}
+                <Button color="primary" onClick={this.toggleNewUserCreateDataModal}>Create+</Button>
+                <Modal isOpen={this.state.newUserCreateDataModal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggleNewUserCreateDataModal}>Create a new user data</ModalHeader>
+                <ModalBody>
+                    <FormGroup>
+                    <Label for="firstname">FirstName</Label>
+                    <Input type="text" id="firstname" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="surname">Surname</Label>
+                    <Input type="text" id="surname" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="dateofbirth">Date of Birth</Label>
+                    <Input type="date" id="dateofbirth" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="gender">Gender</Label>
+                    <Input type="select" name="gender" id="gender" className="ml2">
+                        <option></option>
+                        <option>Male</option>
+                        <option>Female</option>
+                    </Input>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="height">Height(m)</Label>
+                    <Input type="text" id="height" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="haircolor">Hair Color</Label>
+                    <Input id="haircolor" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="weight">Weight</Label>
+                    <Input id="weight" />
+                    </FormGroup>
+                </ModalBody>
+                <ModalFooter>
+                    <Button typr="submit" color="primary" onClick={this.toggleNewUserCreateDataModal}>Submit</Button>{' '}
+                    <Button color="secondary" onClick={this.toggleNewUserCreateDataModal}>Cancel</Button>
+                </ModalFooter>
+                </Modal>
+                {/* end of create modal */}
+
+
                 <table className="table w-100">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
                         <th scope="col">FirstName</th>
                         <th scope="col">Surname</th>
-                        <th scope="col">Actions<button className='ml3'>Create+</button></th>
+                        <th scope="col">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,7 +99,7 @@ export default class Display extends React.Component {
                         <td>Lawal</td>
                         <td>
                             <div className="action">
-                                <button>More...</button>
+                                <button className="more bg-primary" onClick={this.toggleNewUserDisplayDataModal}>More...</button>
                                 <button className='edit'>Edit</button>
                                 <button className='delete'>Delete</button>
                                 </div>
@@ -45,6 +107,46 @@ export default class Display extends React.Component {
                         </tr>
                     </tbody>
                     </table>
+
+                {/* start more modal */}
+                <Modal isOpen={this.state.newUserDisplayDataModal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggleNewUserDisplayDataModal}>Complete User Profile</ModalHeader>
+                <ModalBody>
+                    <FormGroup>
+                    <Label for="firstname" className="b">FirstName:</Label>
+                    <p>Sodiq</p>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="surname" className="b">Surname:</Label>
+                    <p>Lawal</p>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="dateofbirth" className="b">Date Of Birth:</Label>
+                    <p></p>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="gender" className="b">Gender:</Label>
+                    <p>Male</p>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="height" className="b">Height(m):</Label>
+                    <p></p>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="haircolor" className="b">Hair Color:</Label>
+                    <p></p>
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="weight" className="b">Weight:</Label>
+                    <p></p>
+                    </FormGroup>
+                </ModalBody>
+                <ModalFooter>
+                    <Button typr="submit" color="primary" onClick={this.toggleNewUserDisplayDataModal}>Submit</Button>{' '}
+                    <Button color="secondary" onClick={this.toggleNewUserDisplayDataModal}>Cancel</Button>
+                </ModalFooter>
+                </Modal>              
+                {/* end more modal */}
                 </div>
             </React.Fragment>
         )
